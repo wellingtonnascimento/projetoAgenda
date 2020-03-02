e4ea70c35ca8c916027bdba8986f2df5e3b7e68c
Wellington Nascimento <wellingto_07@hotmail.com>
2020-03-02 15:48:24 -0300

    criando criptografia para senha do usuario

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
     email: {type: String, required: true},
@@ -19,6 +20,12 @@ class Login{
     async register(){
         this.valida();
         if(this.errors.length >0) return;
+
+
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
         try {
             this.user = await LoginModel.create(this.body);
         } catch (e) {
@@ -27,6 +34,8 @@ class Login{
         }
        
     }
+
+
     valida(){
         this.cleanUp();
         //Validação
